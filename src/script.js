17d16823b8f28dedf5392d117f3333eaf92a2f10
Bruno Córdova <brunocordovaa@gmail.com>
2020-02-05 21:47:45 -0300

    feat(mag-faq): Style and active state implementation

--- src/script.js
+++ src/script.js
@@ -4,11 +4,24 @@ $(document).ready(function () {
         alert('Click do teste do ' + type + ' registrado!!!');
     };
 
-    //Adicionando texto inicial no mag-faq-title
+    // Adicionando texto inicial no mag-faq-title
     $('.mag-faq-title').text('Cartão de crédito e débito');
 
-    $('.mag-faq .mag-icon-and-text i').on('click', function() {
-        const content = $(this).data('content');
+    // Adicionando a cor branca para o primeiro icone da seção do mag-faq
+    $('.mag-faq .mag-icon-and-text').first().css('color', '#fff');
+
+    $('.mag-faq .mag-icon-and-text').on('click', function() {
+        const content = $(this).find('i').data('content');
         $('.mag-faq-title').text(content);
+
+        // Deixando todos os mag-icon-and-text cinza
+        $('.mag-faq .mag-icon-and-text').css('color', '#a8a8a8');
+
+        $(this).css('color', '#fff');
+         // Deixando a cor do texto em branco
+         $(this).css('color', '#fff');
     });
+
+   
+    
 });
